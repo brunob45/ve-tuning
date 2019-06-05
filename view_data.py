c5b84807a262684f8bb6ab29cd667c39f83b80db
Bruno Bousquet <b.b45@hotmail.com>
2019-06-05 19:16:04 -0400

    tune3 log2

--- view_data.py
+++ view_data.py
@@ -4,12 +4,14 @@ import json
 import sys
 
 thresh = 0.3
+scatter = True
 
 f = open("data.json", "r")
 table = json.load(f)["values"]
 f.close
 
 for j in range(16):
+    plt.figure(num=str(j))
     for i in range(16):
         x = []
         y = []
@@ -17,19 +19,28 @@ for j in range(16):
         s = []
         for z in table[j][i]:
             if z[1] is not None:
-                x.append(len(x)+1)
+                if scatter:
+                    x.append(z[0])
+                else:
+                    x.append(len(x)+1)
+                
                 y.append(z[1])
+
                 if z[1] < -thresh:
                     c.append("red")
                 elif z[1] > thresh:
                     c.append("blue")
                 else:
                     c.append("green")
+
         if len(x) > 0:
             plt.subplot(1,16,i+1)
-            plt.axis([0, len(x)+1, -5, 5])
-            # plt.scatter(x, y, s=5, c=c)
-            plt.plot(x, y)
+            if scatter:
+                plt.scatter(x, y, s=5, c=c)
+                plt.axis([15, 75, -4, 4])
+            else:
+                plt.plot(x, y)
+                plt.axis([0, len(x)+1, -5, 5])
     plt.show()
 
 plt.show()
