fix gentable average calculation

gentable.py
@@ -1,6 +1,7 @@
 import json
 import sys
 import tableloader
+import numpy as np
 
 f = open("data.json", "r")
 table = json.load(f)["values"]
@@ -11,16 +12,15 @@ class Reading:
         self.VE = VE
         self.values = []
         self.avg = 0
+        self.std = 0
 
     def add_value(self, value):
         self.values.append(value)
-        self.avg = 0
-        for v in self.values:
-            self.avg += v
-        v /= len(self.values)
+        self.avg = np.mean(self.values)
+        self.std = np.std(self.values)
     
     def __repr__(self):
-        return str(self.avg)
+        return str(round(self.avg,2))
 
 output = tableloader.load_xml(sys.argv[1])
 
@@ -39,9 +39,9 @@ class Cell:
         have_lower = False
         have_higher = False
         for v in self.values.values():
-            if v.avg < -0.5:
+            if v.avg < 0:
                 have_lower = True
-            elif v.avg > 0.5:
+            elif v.avg > 0:
                 have_higher = True
         return have_lower and have_higher
     
@@ -51,26 +51,38 @@ class Cell:
             if v.avg < 0.5 and v.avg > -0.5:
                 have_exact = True
         return have_exact
+    
+    def __repr__(self):
+        s = ""
+        keys = self.values.keys()
+        while len(keys) > 0:
+            lowest = '999'
+            for key in keys:
+                if int(key) < int(lowest):
+                    lowest = key
+            s += str((lowest, self.values[lowest])) + ','
+            del self.values[lowest]
+        return s
 
 
 for j in range(16):
     for i in range(16):
         new_value = table[j][i][0][0]
         cell = Cell(table[j][i])
-        if cell.is_exact():
-            bestscore = 999
-            bestitem = None
-            for v in cell.values.values():
-                if abs(v.avg) < bestscore:
-                    bestscore = abs(v.avg)
-                    bestitem = (v.VE, v.avg)
-            if bestitem[1] < -0.2:
-                new_value = bestitem[0] - 1
-            elif bestitem[1] > 0.2:
-                new_value = bestitem[0] + 1
-            else:
-                new_value = bestitem[0]
-        elif cell.is_complete():
+        # if cell.is_exact():
+        #     bestscore = 999
+        #     bestitem = None
+        #     for v in cell.values.values():
+        #         if abs(v.avg) < bestscore:
+        #             bestscore = abs(v.avg)
+        #             bestitem = (v.VE, v.avg)
+        #     if bestitem[1] < -0.3:
+        #         new_value = bestitem[0] - 1
+        #     elif bestitem[1] > 0.3:
+        #         new_value = bestitem[0] + 1
+        #     else:
+        #         new_value = bestitem[0]
+        if cell.is_complete():
             max_neg_item = None
             max_neg_score = -999
             min_pos_item = None
@@ -93,7 +105,9 @@ for j in range(16):
                     bestscore = abs(v.avg)
                     bestitem = (v.VE, v.avg)
             if bestitem is not None:
-                new_value = int(round(bestitem[0] + bestitem[1]*3))
+                new_value = bestitem[0] + int(bestitem[1]*3)
         output['z'][j][i] = new_value
+
+        print(cell, new_value)
 
 tableloader.print_xml(sys.argv[1], output)